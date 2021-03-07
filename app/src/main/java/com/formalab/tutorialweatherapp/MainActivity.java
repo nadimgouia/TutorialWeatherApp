928bc2a53f97c70ecf954ba77ddf47a4155ad5a0
Nadim GOUIA <ngouia@telecomdesign.fr>
2021-03-07 21:38:07 +0100

    prepare the ui to get current weather

--- app/src/main/java/com/formalab/tutorialweatherapp/MainActivity.java
+++ app/src/main/java/com/formalab/tutorialweatherapp/MainActivity.java
@@ -3,14 +3,66 @@ package com.formalab.tutorialweatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String API_KEY = "df07195ad38e7d3cbb2f448d3aec3285";
+
+    Button btnSearch;
+    EditText etCityName;
+    ImageView iconWeather;
+    TextView tvTemp, tvCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().hide();
 
+        btnSearch = findViewById(R.id.btnSearch);
+        etCityName = findViewById(R.id.etCityName);
+        iconWeather = findViewById(R.id.iconWeather);
+        tvTemp = findViewById(R.id.tvTemp);
+        tvCity = findViewById(R.id.tvCity);
+
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String city = etCityName.getText().toString();
+                if(city.isEmpty())
+                    Toast.makeText(MainActivity.this, "Please enter a city name", Toast.LENGTH_SHORT).show();
+                else {
+                    // TODO : load weather by city name !
+                    loadWeatherByCityName(city);
+                }
+            }
+        });
+
+    }
+
+    private void loadWeatherByCityName(String city) {
+        Ion.with(this)
+                .load("http://api.openweathermap.org/data/2.5/weather?q="+city+"&&units=metric&appid="+API_KEY)
+                .asJsonObject()
+                .setCallback(new FutureCallback<JsonObject>() {
+                    @Override
+                    public void onCompleted(Exception e, JsonObject result) {
+                        // do stuff with the result or error
+                        Log.d("result", result.toString());
+                    }
+                });
     }
 }
